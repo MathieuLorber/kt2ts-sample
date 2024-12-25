e04ff3543cc34619bd892db0615cecdd165511d0
Mathieu Lorber <mathieu.lorber@gmail.com>
2024-12-25 23:57:53 +0100

    SAVEPOINT

--- kotlin/src/main/kotlin/io/github/kt2tssample/test.kt
+++ kotlin/src/main/kotlin/io/github/kt2tssample/test.kt
@@ -9,6 +9,7 @@ import kotlinx.ast.common.ast.DefaultAstTerminal
 import kotlinx.ast.common.klass.KlassAnnotation
 import kotlinx.ast.common.klass.KlassDeclaration
 import kotlinx.ast.common.klass.KlassIdentifier
+import kotlinx.ast.common.klass.KlassInheritance
 import kotlinx.ast.common.klass.KlassString
 import kotlinx.ast.grammar.kotlin.common.summary
 import kotlinx.ast.grammar.kotlin.common.summary.Import
@@ -25,15 +26,20 @@ fun main() {
       .onSuccess { astList ->
         val packageName = processPackage(astList)
         val imports = processImports(astList)
-        val classes = process(astList)
+        val classes = process(astList).let(::addSealedChildClasses)
         val sb = StringBuilder()
         classes.forEach {
           if (it.isSealed) {
-            sb.append("export type ${it.localName.name} = ")
-            it.sealedSubClasses.forEach { sb.append("| ${it.name}") }
+            sb.append("export type ${it.localName.name} =")
+            it.sealedChildClasses.forEach { sb.append(" | ${it.name}") }
             sb.append(";\n")
+          } else {
+            sb.append("export interface ${it.localName.name} {\n")
+            it.fields.forEach { field -> sb.append("  ${field.fieldName}: ${field.type.name};\n") }
+            sb.append("}\n")
           }
         }
+        dir.resolve("Sealed.ts").toFile().writeText(sb.toString())
       }
       .onFailure { errors -> errors.forEach(::println) }
 }
@@ -65,9 +71,10 @@ data class ClassDeclaration(
     val localName: ClassSimpleName,
     val annotations: List<ClassAnnotation>,
     val isSealed: Boolean,
-    val sealedSubClasses: List<ClassSimpleName>,
+    val sealedChildClasses: List<ClassSimpleName>,
     val parentClasses: List<ClassSimpleName>,
     val fields: List<FieldDeclaration>,
+    // TODO pour choper la selection c'est recursif (attention boucles)
     val selectionOrigins: List<SelectionOrigin>
 )
 
@@ -99,28 +106,32 @@ private fun process(asts: List<Ast>): List<ClassDeclaration> =
           val modifiers = it.modifiers.filter { it.modifier in setOf("sealed", "data") }
           if (modifiers.isEmpty()) return@mapNotNull null
           //            val parents =
-            val annotations = processAnnotations(it.annotations)
-            // TODO qualifiedName
-            val selectionOrigins =
-                if (annotations.any { it.annotation.name == "GenerateTypescript" }) {
-                    listOf(SelectionOrigin.ByAnnotation(ClassSimpleName("GenerateTypescript")))
-                } else {
-                    emptyList()
-                }
-            val parentClasses = processParentClasses(it.children)
-            val fields = processFields(it.parameter)
+          val annotations = processAnnotations(it.annotations)
+          // TODO qualifiedName
+          val selectionOrigins =
+              if (annotations.any { it.annotation.name == "GenerateTypescript" }) {
+                listOf(SelectionOrigin.ByAnnotation(ClassSimpleName("GenerateTypescript")))
+              } else {
+                emptyList()
+              }
+          val parentClasses = processParentClasses(it.children)
+          val fields = processFields(it.parameter)
           ClassDeclaration(
               localName = ClassSimpleName(identifier),
               annotations = annotations,
               isSealed = modifiers.any { it.modifier == "sealed" },
-              sealedSubClasses = emptyList(),
+              sealedChildClasses = emptyList(),
               parentClasses = parentClasses,
               fields = fields,
               selectionOrigins = selectionOrigins,
           )
         }
 
-fun processParentClasses(value: List<Ast>): List<ClassSimpleName> = emptyList()
+fun processParentClasses(children: List<Ast>): List<ClassSimpleName> =
+    children.filterIsInstance<KlassInheritance>().mapNotNull {
+      val identifier = it.type.identifier
+      ClassSimpleName(identifier)
+    }
 
 fun processFields(declarations: List<KlassDeclaration>): List<FieldDeclaration> =
     declarations
@@ -128,11 +139,10 @@ fun processFields(declarations: List<KlassDeclaration>): List<FieldDeclaration> 
         ?.parameter
         ?.filterIsInstance<KlassDeclaration>()
         ?.mapNotNull {
-            val identifier = it.identifier?.identifier ?: return@mapNotNull null
-            val type = it.type?.firstOrNull()?.identifier ?: return@mapNotNull null
-            FieldDeclaration(fieldName = identifier, type = ClassSimpleName(type))
-        }
-        ?: emptyList()
+          val identifier = it.identifier?.identifier ?: return@mapNotNull null
+          val type = it.type?.firstOrNull()?.identifier ?: return@mapNotNull null
+          FieldDeclaration(fieldName = identifier, type = ClassSimpleName(type))
+        } ?: emptyList()
 
 fun processAnnotations(annotations: List<KlassAnnotation>): List<ClassAnnotation> =
     annotations.mapNotNull {
@@ -170,4 +180,16 @@ fun processAnnotations(annotations: List<KlassAnnotation>): List<ClassAnnotation
               }
               .toMap()
       ClassAnnotation(ClassSimpleName(identifier), values)
+    }
+
+fun addSealedChildClasses(classes: List<ClassDeclaration>): List<ClassDeclaration> =
+    classes.map { clazz ->
+      if (clazz.isSealed) {
+        // TODO use qualifiedName
+        val sealedChildClasses =
+            classes.filter { it.parentClasses.contains(clazz.localName) }.map { it.localName }
+        clazz.copy(sealedChildClasses = sealedChildClasses)
+      } else {
+        clazz
+      }
     }
