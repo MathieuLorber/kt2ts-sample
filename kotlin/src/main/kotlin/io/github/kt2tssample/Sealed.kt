0c9c9153999473b7af857ca53ff5a84400c6a54e
Mathieu Lorber <mathieu.lorber@gmail.com>
2024-12-25 12:34:06 +0100

    SAVEPOINT

--- kotlin/src/main/kotlin/io/github/kt2tssample/Sealed.kt
+++ kotlin/src/main/kotlin/io/github/kt2tssample/Sealed.kt
@@ -8,6 +8,17 @@ import kt2ts.annotation.GenerateTypescript
     use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "objectType")
 sealed class SomeSealedClass
 
-data class SomeClassImpl(val someValue: String) : SomeSealedClass()
+data class SomeClassImpl(
+    val someValue: String,
+    val someValue1: String,
+    val someValue2: String,
+    val someValue3: String,
+    val someValue4: String,
+    val someValue5: String,
+    val someValue6: String,
+    val someValue7: String,
+    val someValue8: String,
+    val someValue9: String,
+) : SomeSealedClass()
 
 data class AnotherClassImpl(val anotherValue: Int) : SomeSealedClass()
--- typescript/src/generated/io/github/kt2tssample/Sealed.generated.ts
+++ typescript/src/generated/io/github/kt2tssample/Sealed.generated.ts
@@ -3,6 +3,15 @@ export type SomeSealedClass = AnotherClassImpl | SomeClassImpl;
 export interface SomeClassImpl {
   objectType: 'SomeClassImpl';
   someValue: string;
+  someValue1: string;
+  someValue2: string;
+  someValue3: string;
+  someValue4: string;
+  someValue5: string;
+  someValue6: string;
+  someValue7: string;
+  someValue8: string;
+  someValue9: string;
 }
 
 export interface AnotherClassImpl {
