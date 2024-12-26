8bf7dfe6d789165fec6e766ac6b04dd66abc321d
Mathieu Lorber <mathieu.lorber@gmail.com>
2024-12-26 14:01:20 +0100

    Format

--- typescript/src/utils/nominal-class.ts
+++ typescript/src/utils/nominal-class.ts
@@ -1,22 +1,22 @@
 export type NominalItem = NominalString<any> | NominalNumber<any>;
 
 export type NominalString<T extends string> = string &
-    TypeGuardedNominalString<T>;
+  TypeGuardedNominalString<T>;
 
 abstract class TypeGuardedNominalString<T extends string> {
-    private _typeGuard!: T;
+  private _typeGuard!: T;
 }
 
 export type NominalNumber<T extends string> = number &
-    TypeGuardedNominalNumber<T>;
+  TypeGuardedNominalNumber<T>;
 
 abstract class TypeGuardedNominalNumber<T extends string> {
-    private _typeGuard!: T;
+  private _typeGuard!: T;
 }
 
 type DictKey = NominalItem | string;
 
 export class Dict<K extends DictKey, T> {
-    private _typeGuardKey!: K;
-    private _typeGuardValue!: T;
+  private _typeGuardKey!: K;
+  private _typeGuardValue!: T;
 }
